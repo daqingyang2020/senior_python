cbfaedebc94f126ed5d0821fba98a78ec734196b
daqingyang2020 <73088779+daqingyang2020@users.noreply.github.com>
2021-08-24 18:15:14 +0800

    update

--- demo_8.py
+++ demo_8.py
@@ -102,10 +102,10 @@ def test_value(funny, value):
 # 继承元类type , class type(object) type 继承了object
 class MyMateClass(type):
 
-    # 通过类创建对象
-    def __new__(cls, name, bases, attr, *args, **kwargs):
+    # 通过类创建对象 第一参数cls 推荐写成mcs
+    def __new__(mcs, name, bases, attr, *args, **kwargs):
         # 通过元类创建一个类， 调用父类的方法
-        test_cls = super(MyMateClass, cls).__new__(cls, name, bases, attr)
+        test_cls = super(MyMateClass, mcs).__new__(mcs, name, bases, attr)
         funny = getattr(test_cls, 'perform')
         # 根据传入的测试数据 参数attr ，来遍历这个属性Cases - attr['Cases']
         for index, case in enumerate(attr['Cases']):
@@ -166,8 +166,9 @@ class Custom(type):
 Peter = Custom('Peter', (object, ), {})
 print(getattr(Peter, 'class_name'))
 print(getattr(Peter, 'create_time'))
-# 2、实现上课写的通过元类生成用例的案例代码
 
+# 2、实现上课写的通过元类生成用例的案例代码
+# 见上
 
 """
 说明：以下面试扩展算法题，和上课内容无关，不计分，不是必做题
@@ -184,7 +185,61 @@ print(getattr(Peter, 'create_time'))
 第六次下船，编号为15
 .....
 请问最后那些编号的人下船了？
+"""
 
+people = [person for person in range(1, 41)]
+print(people)
+# 离开的人
+leave = []
+# 初始化第九人
+index = 8
+# 越界
+over = 0
+while len(people) > 20:
+    while index <= len(people):
+        one = people.pop(index)
+        leave.append(one)
+        cur_len = len(people)
+        if cur_len == 20:
+            break
+        index += 8
+        if index >= cur_len:
+            # 越界
+            over = index - cur_len
+            break
+        else:
+            pass
+    # 越界后重置
+    index = over
+print('Total Leave people:', len(leave))
+print(leave)
+
+#
+people2 = [person for person in range(1, 41)]
+
+
+def leave_people(remain):
+    take_off = []
+    flag = 8
+    while len(remain) > 20:
+        each = people2.pop(flag)
+        take_off.append(each)
+        current_len = len(remain)
+        if current_len == 20:
+            break
+        if flag + 8 >= current_len:
+            flag = flag + 8 - current_len
+        else:
+            flag += 8
+    return take_off
+
+
+print('use function:')
+ll = leave_people(people2)
+print(ll, len(ll))
+
+
+"""
 扩展2：
 2 一个球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，
 求它在第10次落地时，共经过多少米？请用递归实现！
@@ -195,8 +250,21 @@ print(getattr(Peter, 'create_time'))
 4    100+100/2*2+100/2*2               +     (100/8)*2    100/4      100/2**(2)
 5    100+100/2*2+100/2*2+100/2*2*2     +     (100/16)*2   100/8      100/2**(3)
 n    上一次的累计距离   +                 +                             (100/2**(n-2))
+"""
 
 
+def fall(height, times):
+    if times == 1:
+        return height
+    else:
+        # n次 = 上一次的累计距离 + (高度/2**(n-2))
+        return fall(height, times-1) + height / 2**(times-2)
+    pass
+
+
+print(fall(100, 10))
+
+"""
 扩展3
 问题：小明有一对刚出生的兔子，兔子的成长期为2个月，从第三个月开始每个月都生一对小兔子，
 子，假如兔子都不小兔子从第三个月开始每个月也会生一对兔死，问n个月后的兔子总数为多少？请用递归实现（意味着生长期为2个月）
@@ -210,6 +278,18 @@ n    上一次的累计距离   +                 +                             
                                                                      26     13
                                                                      42    21
 
-
-扩展4：有时间的小伙伴 可以尝试利用讲的元类的思想 自己封装测试框架
 """
+
+
+def rabbit(month):
+    if month == 1 or month == 2:
+        return 2
+    else:
+        return 2 * (month-2) + rabbit(month-2)
+    pass
+
+
+print(rabbit(6))
+
+# 扩展4：有时间的小伙伴 可以尝试利用讲的元类的思想 自己封装测试框架
+
